c41cb2cd9dcd07780c00aef21eaeec187637320b
HridoyBhuiyan <60400227+HridoyBhuiyan@users.noreply.github.com>
2021-05-26 15:42:18 +0600

    i upload all the Dom item that i know here

--- dom/main.js
+++ dom/main.js
@@ -1,3 +1,4 @@
+//getElementByID
 var ans1 = "Yes i will";
 document.getElementById("ban--h1").innerHTML = "I love Bangladesh";
 document.getElementById("ban--h2").innerHTML = ans1;
@@ -8,3 +9,18 @@ var historicalDate = function ()
 {
     document.getElementById("historical--date").innerHTML="Those are the historical date";
 }
+
+//Queryselector
+var myself = function ()
+{
+    document.querySelector("#mySelf").innerHTML = "I am a progammar too!"
+}
+
+var mySelf1 = function ()
+{
+    for (let i = 0; i <=7; i++) {
+        document.querySelectorAll("#my--self--1 > p")[i].innerHTML = "I Want to become a good programmer";
+    }   
+}
+
+
