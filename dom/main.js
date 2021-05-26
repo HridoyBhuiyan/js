07665be965fa7a71ebfaea025a30662efa3d1477
HridoyBhuiyan <60400227+HridoyBhuiyan@users.noreply.github.com>
2021-05-26 14:54:02 +0600

    Those i have learned about JS DOM

---
+++ dom/main.js
@@ -0,0 +1,10 @@
+var ans1 = "Yes i will";
+document.getElementById("ban--h1").innerHTML = "I love Bangladesh";
+document.getElementById("ban--h2").innerHTML = ans1;
+var ban = function (){
+    document.getElementById("ban--city").innerHTML="Dhaka, Syhlet, CTG, Khulna and others";
+}
+var historicalDate = function ()
+{
+    document.getElementById("historical--date").innerHTML="Those are the historical date";
+}
