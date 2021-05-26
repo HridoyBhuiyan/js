7f210378857380a985c2ebbc4a45d4e4ee69b3fa
HridoyBhuiyan <60400227+HridoyBhuiyan@users.noreply.github.com>
2021-05-26 12:17:31 +0600

    end of math function

---
+++ 29/main.js
@@ -0,0 +1,21 @@
+var a = Math.abs(-2.4);
+console.log(a);
+
+var b = [2, 5, 1.9, 3.5, 25];
+var c = Math.max(b[0], b[1])
+console.log(c)
+
+var c = Math.sqrt(b[4])
+console.log(c);
+
+var c = Math.ceil(b[3]);
+console.log(c);
+
+var c = Math.floor(b[2]);
+console.log(c);
+
+var c = Math.random() * 3;
+console.log(c);
+
+var c = Math.ceil(c);
+console.log(c);
