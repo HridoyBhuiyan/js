e5cd7ebdb449a5cb456f1ff4feb96dd258a6cb1d
HridoyBhuiyan <60400227+HridoyBhuiyan@users.noreply.github.com>
2021-05-27 12:05:40 +0600

    i learned how to add style on js

---
+++ JSCSS/main.js
@@ -0,0 +1,8 @@
+var addStyle = function ()
+{
+    document.getElementById("my--para").className = "text--soul"
+}
+var removeStyle = function ()
+{
+    document.getElementById("my--para").className = "";
+}
